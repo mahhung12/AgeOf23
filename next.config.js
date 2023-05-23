91d5fe386ac46f22ba4c6d8d8a1b4053979837d0
mahhung12 <mahhungg.12@gmail.com>
2023-05-23 17:22:29 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,23 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
   images: {
     disableStaticImages: true,
     domains: ["images.unsplash.com"],
     deviceSizes: [375, 720, 1080],
     imageSizes: [300, 600, 900],
+  },
+  experimental: {
+    serverComponents: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:any*",
+        destination: "/src/:any*",
+      },
+    ];
   },
 };
 
