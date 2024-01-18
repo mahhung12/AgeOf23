f7399f51a95f624f5eacf6643fc4f17cf8f7aefa
hungdm <mahhungg.12@gmail.com>
2024-01-18 16:03:11 +0700

    update mdx file

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -13,7 +13,7 @@ const AppLayout: FC<PropsWithChildren<AppLayoutProps>> = ({ children }) => {
       <div className="max-w-[1600px] w-full h-full m-auto flex align-middle relative">
         <AppSider />
 
-        <div className="w-full h-full p-6 sm:p-8 xl:p-16 lg:w-full lg:flex-shrink text-black dark:text-white">
+        <div className="prosed w-full h-full p-6 sm:p-8 xl:p-16 lg:w-full lg:flex-shrink text-black dark:text-white">
           {children}
         </div>
       </div>
