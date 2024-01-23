8ec9683ada52444a3e4e2b74273534a65c4e2d39
hungdm <mahhungg.12@gmail.com>
2024-01-23 13:50:56 +0700

    fix error build

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,23 +1,25 @@
-import { fetchPageBlocks, fetchPageBySlug, notion } from "@/lib/notion";
-import bookmarkPlugin from "@notion-render/bookmark-plugin";
-import { NotionRenderer } from "@notion-render/client";
-import hljsPlugin from "@notion-render/hljs-plugin";
-import { notFound } from "next/navigation";
+// import { fetchPageBlocks, fetchPageBySlug, notion } from "@/lib/notion";
+// import bookmarkPlugin from "@notion-render/bookmark-plugin";
+// import { NotionRenderer } from "@notion-render/client";
+// import hljsPlugin from "@notion-render/hljs-plugin";
+// import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const post = await fetchPageBySlug(params.slug);
-  if (!post) notFound();
+  // const post = await fetchPageBySlug(params.slug);
+  // if (!post) notFound();
 
-  const blocks = await fetchPageBlocks(post.id);
+  // const blocks = await fetchPageBlocks(post.id);
 
-  const renderer = new NotionRenderer({
-    client: notion,
-  });
+  // const renderer = new NotionRenderer({
+  //   client: notion,
+  // });
 
-  // renderer.use(hljsPlugin({}));
-  // renderer.use(bookmarkPlugin({}));
+  // // renderer.use(hljsPlugin({}));
+  // // renderer.use(bookmarkPlugin({}));
 
-  const html = await renderer.render(...blocks);
+  // const html = await renderer.render(...blocks);
 
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
+  return 1
+
+  // return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
 }
